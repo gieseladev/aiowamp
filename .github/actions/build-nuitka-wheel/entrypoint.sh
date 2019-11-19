d246e3406ecd09c3cebdfc4e649ae5c35f5cdd23
Simon Berger <simon@siku2.io>
2019-11-19 17:28:38 +0100

    pipenv ruined it this time

--- .github/actions/build-nuitka-wheel/entrypoint.sh
+++ .github/actions/build-nuitka-wheel/entrypoint.sh
@@ -2,5 +2,7 @@
 set -e
 
 pipenv install --deploy --dev
-pipenv run build_nuitka -- --dist-dir linux_dist
+pipenv run -- build_nuitka --dist-dir linux_dist
 auditwheel repair linux_dist/*.whl --wheel-dir dist/
+
+ls -R dist
