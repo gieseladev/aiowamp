93610523a420acb11a02d51f3cb2ba4e390be7da
Simon Berger <simon@siku2.io>
2019-11-19 17:22:59 +0100

    welp, you had your chance, let's do it inefficiently then

--- .github/actions/build-nuitka-wheel/entrypoint.sh
+++ .github/actions/build-nuitka-wheel/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-ls -R ${pythonLocation}
-
-PATH="${pythonLocation}/bin:${PATH}"
-
-python setup.py bdist_nuitka --dist-dir linux_dist
+pipenv install --deploy --dev
+pipenv run build_nuitka -- --dist-dir linux_dist
 auditwheel repair linux_dist/*.whl --wheel-dir dist/
