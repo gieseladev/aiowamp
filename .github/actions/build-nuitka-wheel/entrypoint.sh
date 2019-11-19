338d5e081d28b622fee2b1efb4511c6f200ab56e
Simon Berger <simon@siku2.io>
2019-11-19 17:19:16 +0100

    back to python location I guess

--- .github/actions/build-nuitka-wheel/entrypoint.sh
+++ .github/actions/build-nuitka-wheel/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+ls -R ${pythonLocation}
+
+PATH="${pythonLocation}/bin:${PATH}"
+
 python setup.py bdist_nuitka --dist-dir linux_dist
 auditwheel repair linux_dist/*.whl --wheel-dir dist/
