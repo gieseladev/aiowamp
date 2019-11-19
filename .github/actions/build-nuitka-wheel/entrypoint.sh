a5adac6a8083585be9aa4219f28ae2ae1908d53b
Simon Berger <simon@siku2.io>
2019-11-19 18:14:14 +0100

    why stop when it works when you could waste even more time on it?

--- .github/actions/build-nuitka-wheel/entrypoint.sh
+++ .github/actions/build-nuitka-wheel/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+PATH="/opt/python/$1/bin:${PATH}"
+
+pip install --upgrade pipenv
 pipenv install --deploy --dev
 pipenv run -- build_nuitka --dist-dir linux_dist
 auditwheel repair linux_dist/*.whl --wheel-dir dist/
-
-ls -R dist
